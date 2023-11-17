0ecaeeea16b56c07a30a433e262c26ab8b079170
Kinenystuen <kine_on@hotmail.com>
2023-11-17 16:59:27 +0100

    Update sendMessage.js

--- js/utils/sendMessage.js
+++ js/utils/sendMessage.js
@@ -7,15 +7,17 @@ export function sendMessageFun() {
   const message_area_sent = document.getElementById("message_area_sent");
   message_area_sent.style.display = "none";
 
-  function sendMessage() {}
-  sendmessageButton.addEventListener("click", function () {
-    message_area.style.display = "none";
-    message_area_sent.style.display = "block";
-  });
-
   newmailButton.addEventListener("click", function () {
     message_area.style.display = "block";
     message_area_sent.style.display = "none";
   });
 }
 sendMessageFun();
+
+function sendMessage() {}
+  sendmessageButton.addEventListener("click", function () {
+    message_area.style.display = "none";
+    message_area_sent.style.display = "block";
+  });
+
+  sendMessage()
