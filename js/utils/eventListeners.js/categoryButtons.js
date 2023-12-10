4cef362f25b39146bb0399cbb42475216264b0e9
Kinenystuen <kine_on@hotmail.com>
2023-12-10 16:43:33 +0100

    updates

--- js/utils/eventListeners.js/categoryButtons.js
+++ js/utils/eventListeners.js/categoryButtons.js
@@ -5,7 +5,7 @@ import { updateMovieDisplay } from "../updMovieDis.js";
 export function categoryButtons() {
     // Add event listeners to the buttons
     document.getElementById('all-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('all');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = '';
@@ -13,7 +13,7 @@ export function categoryButtons() {
     });
   
     document.getElementById('action-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('Action');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = 'Action';
@@ -21,7 +21,7 @@ export function categoryButtons() {
     });
   
     document.getElementById('kids-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('Kids');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = 'Kids';
@@ -29,26 +29,51 @@ export function categoryButtons() {
     });
   
     document.getElementById('horror-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('Horror');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = 'Horror';
       updCategory(selectedGenre);
     });
     document.getElementById('drama-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('Drama');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = 'Drama';
       updCategory(selectedGenre);
     });
     document.getElementById('comedy-btn').addEventListener('click', async () => {
-      handleButtonClick()
+      handleButtonClick();
       const filteredMovies = await filterMovies('Comedy');
       updateMovieDisplay(filteredMovies);
       const selectedGenre = 'Comedy';
       updCategory(selectedGenre);
     });
+    document.getElementById('sortButton').addEventListener('click', async () => {
+      // Get the selected genre (you can replace 'all' with the desired genre)
+      const selectedGenre = `all`;
+    
+      // Call the filterAndSortMovies function
+      const sortedMovies = await filterMovies(selectedGenre);
+    
+      // Call the displayMovies function to update the displayed movies
+      updateMovieDisplay(sortedMovies, selectedGenre);
+    });
+    // Add an event listener to the sort button
+    document.getElementById('priceLowHigh').addEventListener('click', async () => {
+      // Get the selected category (you can replace 'all' with the desired category)
+      const selectedCategory = `all`;
+
+      // Get the selected sorting criterion
+      const selectedSortBy = 'price'; // You can change this to other criteria like 'title'
+
+      // Call the filterAndSortMovies function with category and sorting criterion
+      const sortedMovies = await filterMovies(selectedCategory, selectedSortBy);
+
+      // Call the displayMovies function to update the displayed movies
+      updateMovieDisplay(sortedMovies, selectedCategory);
+    });
+
   }
 
   function updCategory(selectedGenre) {
--- js/utils/filterCategory.js
+++ js/utils/filterCategory.js
@@ -2,10 +2,29 @@
 
 import { fetchApiSquareEyes } from "../api/squareeyesData.js"; 
 
-// Function to filter movies based on the selected genre
-export async function filterMovies(genre) {
+// Function to fetch and filter/sort movies
+export async function filterMovies(category, sortBy) {
   const allMovies = await fetchApiSquareEyes();
 
-  // Filter movies based on the selected genre
-  return genre === 'all' ? allMovies : allMovies.filter(movie => movie.genre === genre);
+  // Filter by category
+  let filteredMovies = category !== 'all' ? allMovies.filter(movie => movie.genre === category) : allMovies;
+
+  // Sort by the selected criterion (e.g., title, price)
+  switch (sortBy) {
+    case 'title':
+      filteredMovies.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case 'price':
+      filteredMovies.sort((a, b) => {
+        const priceA = a.discountedPrice < a.price ? a.discountedPrice : a.price;
+        const priceB = b.discountedPrice < b.price ? b.discountedPrice : b.price;
+        return priceA - priceB;
+      });
+      break;
+    // Add more cases for other sorting criteria if needed
+    default:
+      break;
+  }
+
+  return filteredMovies;
 }
