ce42e8ca5b3a0c7f7c59d653454cb454b8d08585
Kinenystuen <kine_on@hotmail.com>
2026-03-22 14:31:06 +0100

    Fix image source and alt attributes in displayCheckout function; remove commented-out code

--- js/components/displayCheckout.js
+++ js/components/displayCheckout.js
@@ -13,7 +13,7 @@ export async function displayCheckout() {
     loadCheckout.innerHTML += `
                               <div class="gridfit200">
                                 <h2>${movies.title}</h2>
-                                <img src="${movies.image}" alt="${movies.title}" />
+                                <img src="${movies.image.url}" alt="${movies.image.alt}" />
                                 <button>Start Watching</button>
                               </div>
     `;
@@ -21,36 +21,8 @@ export async function displayCheckout() {
 
 }
 
-displayCheckout();
+const checkoutContainer = document.getElementById("checkoutSucsess");
 
-
-// Function to handle the "paynow" button click
-
-// document.getElementById("paynow").addEventListener("click", function () {
-//   const shoppingBag = getExistingShopInv();
-//   const paidMovies = getExistingPaidMovies() || [];
-//   paidMovies.push(...shoppingBag);
-//   localStorage.setItem("paidMovies", JSON.stringify(paidMovies));
-//   displayPaidMovies();
-// });
-
-
-
-// // Function to retrieve existing paid movies from local storage
-// function getExistingPaidMovies() {
-//   return JSON.parse(localStorage.getItem("paidMovies")) || [];
-// }
-
-
-// // Function to fetch and display paid movies at the specified container
-// export function displayPaidMovies() {
-//   const paidMoviesContainer = document.getElementById("purchase_movies_container");
-//   const paidMovies = getExistingPaidMovies();
-//   paidMoviesContainer.innerHTML = '';
-//   paidMovies.forEach(movie => {
-//     const movieDiv = document.createElement('div');
-//     movieDiv.textContent = movie.title; // Adjust this based on your movie object structure
-//     paidMoviesContainer.appendChild(movieDiv);
-//   });
-// }
-
+if (checkoutContainer) {
+  displayCheckout();
+}
