0125ea154dd3268e6a84ea2e18d8f26693c0cdee
Kinenystuen <kine_on@hotmail.com>
2026-03-22 15:45:23 +0100

    Refactor itemtocart tests for improved clarity and consistency in product fetching and shopping bag interactions

--- cypress/e2e/3-testing/itemtocart.cy.js
+++ cypress/e2e/3-testing/itemtocart.cy.js
@@ -1,45 +1,50 @@
-describe('E-commerce Application - Product Fetching', () => {
-    beforeEach(() => {
-        // This will ignore any uncaught exceptions in the application code
-        cy.on('uncaught:exception', (err, runnable) => {
-          // returning false here prevents Cypress from
-          // failing the test
-          return false;
-        });
-    
-        // Visit the page where the movies are listed
-        cy.visit('/movie-pages/all-movies.html');
-      });
-    it('should fetch and display products from the API then find the movie godzilla and add it to the shopping bag', () => {
-  
-      // Wait for the API call to complete and products to be displayed
-      cy.intercept('GET', 'https://api.noroff.dev/api/v1/square-eyes').as('fetchProducts');
-      cy.wait('@fetchProducts');
+describe("E-commerce Application - Product Fetching", () => {
+  beforeEach(() => {
+    cy.on("uncaught:exception", () => false);
 
-      cy.get('.movie_card').contains('h2', 'Godzilla').closest('.movie_card').within(() => {
-        // Find the "Add to cart" button and click it
-        cy.get('.shopping_bag_button.add', { timeout: 10000 }).click();
-      });
-      cy.get('.movie_card').contains('h2', 'Godzilla').closest('.movie_card').within(() => {
-        // Find the "Add to cart" button and click it
-        cy.get('.shopping_bag_button.yellow', { timeout: 10000 }).click();
-      });
-    });
-    it(`Should find mandoloran and add to shopping bag, then remove`, () => {
-        // find the movie with the name mandalorian
-        cy.get('.movie_card').contains('h2', 'Mandalorian').closest('.movie_card').within(() => {
-            // Find the "Add to cart" button and click it
-            cy.get('.shopping_bag_button.add', { timeout: 10000 }).click();
-          });
-          
-          // find shopping bag, and go to the shopping bag page
-          cy.get('a[aria-label="Go to shopping bag"]').click();
-          
-          
-    })
-    it(`Checkout`, () => {
-        // find checkout button
-        cy.get('label').contains(`a`,`Pay now`).click();
-    })
+    cy.intercept("GET", "**/square-eyes").as("fetchProducts");
+    cy.visit("/movie-pages/all-movies.html");
+    cy.wait("@fetchProducts");
   });
-  
+
+  it("should fetch and display products from the API, find Godzilla, and add it to the shopping bag", () => {
+    cy.contains(".movie_card h2", "Godzilla", { timeout: 10000 })
+      .closest(".movie_card")
+      .within(() => {
+        cy.get(".shopping_bag_button", { timeout: 10000 })
+          .should("be.visible")
+          .click();
+      });
+
+    cy.contains(".movie_card h2", "Godzilla")
+      .closest(".movie_card")
+      .within(() => {
+        cy.get(".shopping_bag_button.yellow", { timeout: 10000 }).should(
+          "exist"
+        );
+      });
+  });
+
+it('should find Mandalorian, add it to the shopping bag, then go to checkout page', () => {
+  cy.contains('.movie_card h2', 'Mandalorian', { timeout: 10000 })
+    .closest('.movie_card')
+    .within(() => {
+      cy.get('.shopping_bag_button', { timeout: 10000 })
+        .should('be.visible')
+        .click();
+    });
+
+  cy.get('a[aria-label="Go to shopping bag"]')
+    .scrollIntoView()
+    .click({ force: true });
+
+  cy.url().should('include', '/checkout');
+});
+
+  it("should show the Pay now link or button on checkout page", () => {
+    cy.visit("/checkout.html");
+    cy.contains("a, button", "Pay now", { timeout: 10000 }).should(
+      "be.visible"
+    );
+  });
+});
